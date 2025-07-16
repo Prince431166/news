e99a79b106e2294043a38c5389d3c52dded7df67
Prince431166 <princexharmaji@gmail.com>
2025-07-16 11:57:34 +0530

    Update server.cjs

--- server.cjs
+++ server.cjs
@@ -86,6 +86,8 @@ const PORT = process.env.PORT || 3000; // FIX: Corrected logical OR operator '||
 
 // --- Middleware ---
 app.use(cors({
+    app.use(bodyparser.json({ limit: '100mb' }));
+    app.use(bodyparser.urlencoded({ extended: true, limit: '100mb' }));
     origin: 'https://flashnews1.netlify.app', // **आपका Netlify डोमेन**
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // FIX: Added missing array values for methods
     allowedHeaders: ['Content-Type', 'Authorization'], // FIX: Added missing array values for allowedHeaders
